6d3d27e5dd408e2f94a65c5da13573d196a61426
EvgeniaM6 <evgminukova6@gmail.com>
2022-08-08 12:17:29 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -39,6 +39,43 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = []
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i + 10))
+    }
+
+    let lettersArr = arr.map((item) => {
+        if (item.includes('*')) {
+            return ' '
+        } else {
+            let oneBitLetterArr = []
+            for (let i = 0; i < item.length; i += 2) {
+                oneBitLetterArr.push(item.slice(i, i + 2))
+            }
+
+            let oneMorseLetterArr = oneBitLetterArr.map((item) => {
+                return decodeBitsToMorse(item)
+            })
+
+            let oneMorseLetter = oneMorseLetterArr.join('')
+            let decodedLetter = decodeMorseToWord(oneMorseLetter)
+            return decodedLetter
+        }
+    })
+
+    return lettersArr.join('')
+}
+
+function decodeBitsToMorse(str) {
+    switch (str) {
+        case '00': return '';
+        case '10': return '.';
+        case '11': return '-';
+    }
+}
+
+function decodeMorseToWord(str) {
+    return MORSE_TABLE[str]
 }
 
 module.exports = {
